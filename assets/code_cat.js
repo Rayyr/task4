fa3d7f150a32756d214d66f2b2ce99a8cc61f36e
Rayyr <khasaatirayaa@gmail.com>
2025-03-08 00:19:24 +0200

    n

--- assets/code_cat.js
+++ assets/code_cat.js
@@ -1,8 +1,15 @@
-showCategory();
+
 
 async function getCategory() {
+    let response;
+    try {
+        response = await fetch("https://fakestoreapi.com/products/categories", { method: "get" });
+    }
+    catch (occured_error) {
+        console.log("Sorry , Try Again Later :)");
+        return;
 
-    const response = await fetch("https://fakestoreapi.com/products/categories", { method: "get" });
+    }
 
     return await response.json();
 
@@ -12,11 +19,17 @@ async function showCategory() {
 
     const realData = await getCategory();
 
-    //as same as using the loop since there are no details found for each category 
-    //each passed parameter to the function is about element of the array realData
-    const categoriesWillBeAdded = realData.map(function (category) {
+    if (realData == undefined) {
+        console.log("No Categories found :)");
+        return;
+    }
 
-        let cat = `
+    try {
+        //as same as using the loop since there are no details found for each category 
+        //each passed parameter to the function is about element of the array realData
+        const categoriesWillBeAdded = realData.map(function (category) {
+
+            let cat = `
         <div class="cat">
            <h1>${category}</h1>
            <div class="details">
@@ -25,46 +38,52 @@ async function showCategory() {
         
         </div>`;
 
-        return cat;//the category will be added to the categories list (categoriesWillBeAdded)
-    }).join(''); // to erase the commas ,
+            return cat;//the category will be added to the categories list (categoriesWillBeAdded)
+        }).join(''); // to erase the commas ,
 
 
-    document.querySelector(".cont .container .row").innerHTML =  categoriesWillBeAdded;
-    mani();
+        document.querySelector(".cont .container .row").innerHTML = categoriesWillBeAdded;
+        document.querySelector(".loading").classList.add("no_display");
+        mani();
+
+    }
+    catch (occured_error) {
+        console.log("Sorry , Try Again Later :)");
+        return;
+    }
 
 
-    let parent=document.querySelector(".cont");//parentNode
-    let category1=document.querySelectorAll(".cat");
-    
-    //console.log(category1);
-    //to manipulate the neww added items ---->jquery
 }
 
-function mani(){
-    let r=document.querySelectorAll(".cat");
-   
-  
-    r.forEach(function(ele){
-       
-        
-         ele.onmouseenter=function(){
-        
-          let a=ele.children[1] ;//details child
-         
-          a.classList.toggle("togle");
-          console.log(a);
-         }
+function mani() {
+    let r = document.querySelectorAll(".cat");
 
-         ele.onmouseleave=function(){
-        
-          let a=ele.children[1];
-          a.classList.toggle("togle");
-          console.log(a);
-          
-         } 
+
+    r.forEach(function (ele) {
+
+
+        ele.onmouseenter = function () {
+
+            let a = ele.children[1];//details child
+
+            a.classList.add("toggle");//or toggle
+
+        }
+
+        ele.onmouseleave = function () {
+
+            let a = ele.children[1];
+            a.classList.remove("toggle");//or toggle
+
+
+        }
     })
-    console.log(8);
-   
+
+
 }
+ 
+showCategory(); 
 
  
+ 
+
--- assets/code_product.js
+++ assets/code_product.js
@@ -1,73 +1,241 @@
+function stars() {
 
-let starts=function addStarts(){
-       
-    let count=document.querySelectorAll(".product-rating");
-   
-     console.log(count);
+    let count = document.querySelectorAll(".product-rating");
 
-    for(i=0;i<count.length;i++){
-        let num=Number(count[i].textContent);
-        for(j=0;j<num;j++){
-         let startToadd=`<i class="fa-regular fa-star"></i>`;
-        count[i].innerHTML+=startToadd;
+    for (i = 0; i < count.length; i++) {
+        let num = Number(count[i].textContent);
+        for (j = 0; j < num; j++) {
+            let startToadd = `<i class="fa-regular fa-star"></i>`;
+            count[i].innerHTML += startToadd;
         }
     }
-   
+
 
 };
 
-
-
-async function getProduct() {
-
+ var itemsTotalNum=0;
+async function getProduct(page) {
+    let response;
+    let limit=2;
+    const skippedElements=(page-1)*limit ;
     /* the products will be shown are depends on the clicked category so the category will be variable 
        in the URL */
-    const temp = new URLSearchParams(window.location.search);
-    const catVar = temp.get('catValue');//to extract the catValue from URL
-    const response = await fetch(`https://fakestoreapi.com/products/category/${catVar}`, { method: "GET" });
-    const realData = await response.json();
+    try {
+        const temp = new URLSearchParams(window.location.search);
+        const catVar = temp.get('catValue');//to extract the catValue from URL
+        response = await fetch(`https://fakestoreapi.com/products/category/${catVar}?limit=2&skip=${skippedElements}`, { method: "GET" });
+        w = await fetch(`https://fakestoreapi.com/products/category/${catVar}`, { method: "GET" });
 
-    return realData;
+        items=await w.json() ;
+        itemsTotalNum=items.length;
+        
+    }
+    catch (occured_error) {
+        console.log("Sorry , Try Again Later :)");
+        return;
+    }
+    
+    return await response.json();
+    //limit : 2items per page
+    
 
 }
 
-async function displayProductsOfCategory() {
 
-    const realData = await getProduct();
-    document.querySelector(".navbar .container ").innerHTML = `<h1>${realData[0].category}</h1>`;
 
-    const productWillBeAdded = realData.map(function (product) {
+async function displayProductsOfCategory(page=1) {
 
-        let pro = `<div class="product">
+    console.log(page);
+    const realData = await getProduct(page);
+    let numOfPages=itemsTotalNum /2;
+
+    
+
+    if (realData == undefined) {
+        console.log("No Products found :)");
+        return;
+    }
+
+    try {
+        document.querySelector(".navbar .container ").innerHTML = `<h1>${realData[0].category}</h1>`;
+
+        const productWillBeAdded = realData.map(function (product) {
+
+            let pro = `<div class="product">
         
-        <div class="product-image">
+        <div class="product-image"> 
+        
         <img src="${product.image}" />
+           <div class="overlay">
+             <p class="overlay-text"> Click Me </p>  
+            </div>
         </div>
 
         <div class="product-description">
-     
         <p class="product-title">${product.title}</p>
         <p class="product-details">${product.description}</p>
+
+        <div class="price">
         <p>${product.price} $</p>
-        <p class="product-rating">${product.rating.rate}</p>
-      
+        <p class="product-rating">${product.rating.rate} </p>
         <p>${product.rating.count}</p>
+        </div>
+
         </div >
 
         </div>`;
 
-        return pro;
+            return pro;
 
-    }).join('');
+        }).join('');
 
-   
-    //document here mean : the linked html file wih js file
-    document.querySelector(".cont .container ").innerHTML = productWillBeAdded;
+
+        //document here mean : the linked html file wih js file
+        document.querySelector(".cont .container ").innerHTML = productWillBeAdded;
+        document.querySelector(".loading").classList.add("no_display");
+
+        stars();
+
+        let butnsOfPages=document.querySelector(".pagination ul");
+        butnsOfPages.innerHTML =`<li><button>&lt</button></li>`;
+        for(let i=1;i<=numOfPages;i++){
+
+            let btn=`<button>${i}</button>`;
+            let litem=`<li>${btn}</li>`;
+         
+  
+            butnsOfPages.innerHTML+=litem;
+         
+            
+        }
+        butnsOfPages.innerHTML+=`<li><button>&gt</button></li>`;
+
+         
+        let btns=document.querySelectorAll(".pagination ul button");
+        for(let i=1;i<(btns.length-1);i++){
+           
+           btns[i].addEventListener("click",( )=>{
+            console.log(btns[i]);
+            console.log(i);
+           displayProductsOfCategory(i);
+           });
+        } 
+        
+       
+
+    }
+    catch (occured_error) {
+        console.log("Sorry , Try Again Later :)");
+        return;
+    }
+
+
+    let overlay_layers = document.querySelectorAll(".cont .container .product  .product-image .overlay");
+
+    add_overlay_event_listener(overlay_layers);
+}
+
+
+function add_overlay_event_listener(overlay_layers) {
+
+
+    let x, y, ww, hh;
+    let gModal = document.querySelector(".modal");
+    let imgFrame = document.querySelector(".modal .img");//white box
+    let body = document.querySelector("body");
+    let childs = imgFrame.children;//actually 2 children
+    let rect;
+    let scale = 1;
+    
+
+    for ( let i = 0; i < overlay_layers.length; i++) {
+
+
+        overlay_layers[i].addEventListener("click", (e) => {
+
+            gModal.classList.add("modalDisplay");
+            body.classList.add("disableBodyScroll");
+            let imgToDisplay = overlay_layers[i].previousElementSibling;//actual image 
+            childs[0].setAttribute("src", imgToDisplay.getAttribute("src"));
+
+            let subChilds = childs[1].children;//controlers +/-
+            zooming(subChilds[0], subChilds[1], childs[0], scale);
+
+
+            rect = imgFrame.getBoundingClientRect();
+            x = rect.x;
+            y = rect.y;
+            ww = rect.width;
+            hh = rect.height;
+
+        
+
+        });
+
+    }
+
+    document.addEventListener("mousemove", (e2) => {
+        if (gModal.classList.contains("modalDisplay") == true) {
+
+            let mx = e2.clientX;
+            let my = e2.clientY;
+
+            if (!((mx >= x && mx <= (x + ww)) && (my >= y && my <= (y + hh)))) {
+                document.body.style.cursor = "pointer"
+            }
+            else
+                document.body.style.cursor = "default";
+        }
+    });
+
+    document.addEventListener("click", (e) => {
+
+
+        if (e.target.className != "overlay") {
+
+            let mx = e.clientX;
+            let my = e.clientY;
+
+
+            if (!((mx >= x && mx <= (x + ww)) && (my >= y && my <= (y + hh)))) {
+
+                //out of white madal box range 
+                body.classList.remove("disableBodyScroll");
+                gModal.classList.remove("modalDisplay");
+
+
+
+                scale = 1;
+                childs[0].style.transform = `scale(${scale})`;
+
+
+
+            }
+        }
+
+    });
+
+
+
 
 
 }
 
+function zooming(zoomIn, zoomOut, img, scale) {
+
+
+    zoomIn.addEventListener("click", function zoomIn() {
+        scale += 0.1;
+        if (scale > 1.5) scale = 1;
+        img.style.transform = `scale(${scale})`;
+    });
+
+    zoomOut.addEventListener("click", function zoomOut() {
+        scale -= 0.1;
+        if (scale < .5) scale = 1;
+        img.style.transform = `scale(${scale})`;
+    });
+}
 
 displayProductsOfCategory();
-starts();
  
